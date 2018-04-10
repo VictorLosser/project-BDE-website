a75d61c8dec09cadf7bb885b3148ee87c2796d86
VictorLosser <victor.losser@viacesi.fr>
2018-04-10 15:47:13 +0200

    Add functionality to remove a product from the DB

--- resources/views/product_management/add.blade.php
+++ resources/views/product_management/add.blade.php
@@ -4,55 +4,56 @@
 
 @section('content')
 
+    @if (session('status'))
+        <div class="alert alert-success">
+            {{ session('status') }}
+        </div>
+    @endif
+
     <div id="formProduit">
-        @if (isset($_POST['productName']) AND isset($_POST['productImg']) AND isset($_POST['productDescription']) AND isset($_POST['productPrice']))
-            <p>Félicitations !!! La BDD s'est vu ajouté l'entrée {{ $_POST['productName'] }} </p>
-        @else
+        <div class="alert alert-info" role="alert">
+            Vous êtes sur une page réservée aux administrateurs. (vous en avez de la chance)
+        </div>
 
-            <div class="alert alert-info" role="alert">
-                Vous êtes sur une page réservée aux administrateurs. (vous en avez de la chance)
-            </div>
-
-            <form method="post" action="/ajouter-un-produit/nouveau-produit">
-                {{ csrf_field() }}
-                <div class="row">
-                    <div class="col">
-                        <input name="productName" type="text" class="form-control" placeholder="Nom du produit"
-                               required>
-                    </div>
-                    <div class="col">
-                        <select name="productImg" id="inputState" class="form-control" required>
-                            <option value="" selected>Choissiez une image</option>
-                            <?php
-                            if ($dossier = opendir(public_path('/products'))) {
-                                while (false !== ($fichier = readdir($dossier))) {
-                                    if ($fichier != '.' && $fichier != '..' && $fichier != 'index.php') {
-                                        echo "<option value=\"" . $fichier . "\">" . $fichier . "</option>";
-                                    }
-                                }
-                                closedir($dossier);
-                            }
-                            ?>
-                        </select>
-                    </div>
+        <form method="post" action="/ajouter-un-produit/nouveau-produit">
+            {{ csrf_field() }}
+            <div class="row">
+                <div class="col">
+                    <input name="productName" type="text" class="form-control" placeholder="Nom du produit"
+                           required>
                 </div>
-                <br>
-                <div class="row">
-                    <div class="col">
+                <div class="col">
+                    <select name="productImg" id="inputState" class="form-control" required>
+                        <option value="" selected>Choissiez une image</option>
+                        <?php
+                        if ($dossier = opendir(public_path('/products'))) {
+                            while (false !== ($fichier = readdir($dossier))) {
+                                if ($fichier != '.' && $fichier != '..' && $fichier != 'index.php') {
+                                    echo "<option value=\"" . $fichier . "\">" . $fichier . "</option>";
+                                }
+                            }
+                            closedir($dossier);
+                        }
+                        ?>
+                    </select>
+                </div>
+            </div>
+            <br>
+            <div class="row">
+                <div class="col">
                     <textarea name="productDescription" class="form-control" rows="3" placeholder="description"
                               required></textarea>
-                    </div>
-                    <div class="col">
-                        <input name="productPrice" type="number" class="form-control" placeholder="prix" step="0.01"
-                               required>
-                    </div>
                 </div>
-                <br>
-                <input type="submit"
-                       value="ENVOYER VOTRE NOUVEAU PRODUIT EN APPUYANT SUR CE BOUTON QUI EST BEAUCOUP TROP LONG AHHH"
-                       class="btn btn-sm btn-secondary"/>
-            </form>
-        @endif
+                <div class="col">
+                    <input name="productPrice" type="number" class="form-control" placeholder="prix" step="0.01"
+                           required>
+                </div>
+            </div>
+            <br>
+            <input type="submit"
+                   value="ENVOYER VOTRE NOUVEAU PRODUIT EN APPUYANT SUR CE BOUTON QUI EST BEAUCOUP TROP LONG AHHH"
+                   class="btn btn-sm btn-secondary"/>
+        </form>
     </div>
 
     <br>
--- resources/views/product_management/delete.blade.php
+++ resources/views/product_management/delete.blade.php
@@ -4,27 +4,37 @@
 
 @section('content')
 
-    @if (isset($_POST['choixProduit']))
-        echo "<p>Félicitations !!! La BDD s'est vu retiré l'entrée " . $_POST['choixProduit'];
-    @else
+    <div class="alert alert-info" role="alert">
+        Vous êtes sur une page réservée aux administrateurs. (vous en avez de la chance)
+    </div>
 
-        <form method="post" action="supprimer-un-produit">
-            <div class="alert alert-info" role="alert">
-                Vous êtes sur une page réservée aux administrateurs. (vous en avez de la chance)
-            </div>
-            <select name="choixProduit" id="inputState" class="form-control" required>
-                <option value="" selected>Choissiez un produit</option>
-                @foreach($products as $product)
-
-                    <option value="{{ $product->title }}">{{ $product->title }}</option>
-
-                @endforeach
-            </select>
-
-            <br>
-            <input type="submit" value="SUPPRIMER LE PRODUIT SELECTIONNE" class="btn btn-sm btn-secondary"/>
-        </form>
-
+    @if (session('status'))
+        <div class="alert alert-success">
+            {{ session('status') }}
+        </div>
     @endif
+
+    <div table-responsive-md>
+        <table class="table table-striped">
+            <thead class="thead-light">
+            <th scope="col">ID</th>
+            <th scope="col">Titre</th>
+            <th scope="col">Description</th>
+            <th scope="col">Prix</th>
+            <th scope="col">Action</th>
+            </thead>
+            <tbody>
+            @foreach($products as $product)
+                <tr>
+                    <th>{{ $product->id }}</th>
+                    <td>{{ $product->title }}</td>
+                    <td>{{ $product->description }}</td>
+                    <td>{{ $product->price }}</td>
+                    <td><a href="/supprimer-un-produit/{{ $product->id }}">Supprimer</a></td>
+                </tr>
+            @endforeach
+            </tbody>
+        </table>
+    </div>
 
 @endsection
